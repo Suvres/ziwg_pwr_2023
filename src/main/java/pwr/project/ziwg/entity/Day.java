beec11d72f789f64168495452d076efd0277ef15
Suvres <bartosz.blyszcz@gmail.com>
2023-05-15 19:15:56 +0200

    poprawki

--- src/main/java/pwr/project/ziwg/entity/Day.java
+++ src/main/java/pwr/project/ziwg/entity/Day.java
@@ -1,28 +1,43 @@
 package pwr.project.ziwg.entity;
 
+import com.google.cloud.Date;
+import com.google.cloud.Timestamp;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import pwr.project.ziwg.enums.Emotions;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.Map;
 
-@Getter
 @Setter
+@Getter
 @ToString
 public class Day {
     private String description;
-    private LocalDate date;
-    private Map<String, Emotions> emotions;
+    private String date;
+    private Map<String, Emotions> emotions = new HashMap<>();
 
     @Override
     public boolean equals(Object obj) {
-        if(obj == null || !this.getClass().equals(obj.getClass())) {
+        if (obj == null || !this.getClass().equals(obj.getClass())) {
             return false;
         }
 
         return this.date.equals(((Day) obj).date);
+    }
+
+    public String getDate() {
+        return LocalDate.parse(date).format(DateTimeFormatter.ISO_DATE);
+    }
+
+    public void setDate(String date) {
+        this.date = LocalDate.parse(date).format(DateTimeFormatter.ISO_DATE);
+    }
+
+    public boolean equalDate(LocalDate dateToEqual) {
+       return LocalDate.parse(date).equals(dateToEqual);
     }
 }
