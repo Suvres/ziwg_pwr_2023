d25fd01e8acc0607ea985b78b2ca047587b04f27
Suvres <bartosz.blyszcz>
2023-05-24 21:03:32 +0200

    [FIX] fix cors

--- src/main/java/pwr/project/ziwg/controller/Test.java
+++ src/main/java/pwr/project/ziwg/controller/Test.java
@@ -1,9 +1,11 @@
 package pwr.project.ziwg.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class Test {
 
